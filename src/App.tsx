b412f32da13c9139d40994766453befc561b04ec
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2024-02-20 17:10:34 -0300

    Created and Configurated
    This initial commit establishes the basic structure of the SoftClever.com.br project and includes the following changes:
    
    Development Environment Setup:
    
    Utilized Create React App to create the React development environment.
    Installed and configured Tailwind CSS to facilitate quick and responsive styling of the project.
    Integration of Linters:
    
    Added ESLint to ensure code consistency and quality.
    Configured Prettier to maintain a uniform and readable code style.
    These modifications establish an initial foundation for the project, providing the necessary tools for efficient development and maintenance of the code.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<>
+			<div className="bg-primary w-12 h-12">
+				<h1 className="text-3xl font-bold underline">oie</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
