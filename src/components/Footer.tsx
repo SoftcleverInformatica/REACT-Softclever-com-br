86c3de5c6dc7f90d1e9c49382fef07d2c4325753
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2024-02-26 11:33:25 -0300

    ADD: Whatsapp Button
    This update introduces the addition of a WhatsApp button component, enhancing user engagement and facilitating communication with Soft Clever. The changes made include:
    
    Integration of WhatsApp Button: Implemented a new component named Whatsapp, which includes a WhatsApp icon linked to the Soft Clever WhatsApp account. This button allows users to initiate conversations directly from the website.
    
    Placement and Styling: Positioned the WhatsApp button at the bottom right corner of the screen for easy accessibility. Applied styling to ensure the button stands out against the background.
    
    Static Link: Created a fixed link to the Soft Clever WhatsApp account with a pre-defined message, enabling users to initiate conversations with a pre-filled message indicating their interest in requesting a quote.
    
    This commit enhances the website's functionality by providing users with a convenient way to connect with Soft Clever via WhatsApp directly from the homepage.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"
 
 function Footer() {
 	return (
-		<footer className="bg-second flex w-full flex-col items-center lg:flex-row lg:flex-wrap lg:justify-center">
+		<footer className="flex w-full flex-col items-center bg-second lg:flex-row lg:flex-wrap lg:justify-center">
 			<div className="flex w-full max-w-lg flex-col gap-4 px-8 py-8">
-				<h1 className="text-first text-lg font-black sm:text-xl">REDES SOCIAIS</h1>
-				<p className="text-first text-ms font-medium opacity-80 sm:text-sm">Siga-nos e fique por dentro das novidades.</p>
+				<h1 className="text-lg font-black text-first sm:text-xl">REDES SOCIAIS</h1>
+				<p className="text-ms font-medium text-first opacity-80 sm:text-sm">Siga-nos e fique por dentro das novidades.</p>
 				<div className="flex">
 					<div className="flex h-7 w-10 items-center justify-center bg-[#3B5998]">
 						<FontAwesomeIcon icon={faFacebookF} style={{ color: "#ffffff" }} />
@@ -21,35 +21,35 @@ function Footer() {
 				</div>
 			</div>
 			<div className="flex w-full max-w-lg flex-col gap-4 px-8 py-8">
-				<h1 className="text-first text-lg font-black sm:text-xl">CONTATO</h1>
+				<h1 className="text-lg font-black text-first sm:text-xl">CONTATO</h1>
 				<div className="flex w-full justify-between gap-1">
-					<p className="text-first flex-shrink-0 text-ms font-bold sm:text-sm">Endereço</p>
-					<div className="border-third h-4 flex-grow border-b border-dotted"></div>
-					<p className="text-first flex-shrink-0 text-ms font-medium opacity-80 sm:text-sm">Rua João Alves Pimenta, 880</p>
+					<p className="flex-shrink-0 text-ms font-bold text-first sm:text-sm">Endereço</p>
+					<div className="h-4 flex-grow border-b border-dotted border-third"></div>
+					<p className="flex-shrink-0 text-ms font-medium text-first opacity-80 sm:text-sm">Rua João Alves Pimenta, 880</p>
 				</div>
 				<div className="flex w-full justify-between gap-1">
-					<div className="border-third h-4 flex-grow border-b border-dotted"></div>
-					<p className="text-first flex-shrink-0 text-ms font-medium opacity-80 sm:text-sm">Freguesia do Ó - São Paulo - SP</p>
+					<div className="h-4 flex-grow border-b border-dotted border-third"></div>
+					<p className="flex-shrink-0 text-ms font-medium text-first opacity-80 sm:text-sm">Freguesia do Ó - São Paulo - SP</p>
 				</div>
 				<div className="flex w-full justify-between gap-1">
-					<p className="text-first flex-shrink-0 text-ms font-bold sm:text-sm">E-mail</p>
-					<div className="border-third h-4 flex-grow border-b border-dotted"></div>
-					<p className="text-first flex-shrink-0 text-ms font-medium opacity-80 sm:text-sm">softclever@softclever.com.br</p>
+					<p className="flex-shrink-0 text-ms font-bold text-first sm:text-sm">E-mail</p>
+					<div className="h-4 flex-grow border-b border-dotted border-third"></div>
+					<p className="flex-shrink-0 text-ms font-medium text-first opacity-80 sm:text-sm">softclever@softclever.com.br</p>
 				</div>
 				<div className="flex w-full justify-between gap-1">
-					<p className="text-first flex-shrink-0 text-ms font-bold sm:text-sm">Telefones</p>
-					<div className="border-third h-4 flex-grow border-b border-dotted"></div>
-					<p className="text-first flex-shrink-0 text-ms font-medium opacity-80 sm:text-sm">11 3991-3923 / 11 2361-8334</p>
+					<p className="flex-shrink-0 text-ms font-bold text-first sm:text-sm">Telefones</p>
+					<div className="h-4 flex-grow border-b border-dotted border-third"></div>
+					<p className="flex-shrink-0 text-ms font-medium text-first opacity-80 sm:text-sm">11 3991-3923 / 11 2361-8334</p>
 				</div>
 				<div className="flex w-full justify-between gap-1">
-					<p className="text-first flex-shrink-0 text-ms font-bold sm:text-sm">Atendimento</p>
-					<div className="border-third h-4 flex-grow border-b border-dotted"></div>
-					<p className="text-first flex-shrink-0 text-ms font-medium opacity-80 sm:text-sm">Seg - Sex | 08h - 18h</p>
+					<p className="flex-shrink-0 text-ms font-bold text-first sm:text-sm">Atendimento</p>
+					<div className="h-4 flex-grow border-b border-dotted border-third"></div>
+					<p className="flex-shrink-0 text-ms font-medium text-first opacity-80 sm:text-sm">Seg - Sex | 08h - 18h</p>
 				</div>
 			</div>
-			<div className="border-first flex w-full items-center justify-center gap-4 border-t border-opacity-20 p-4">
-				<p className="text-first text-xs">© 1994 - {new Date().getFullYear()} | Soft Clever</p>
-				<Link to="/politica-de-privacidade" className="text-third text-xs">
+			<div className="flex w-full items-center justify-center gap-4 border-t border-first border-opacity-20 p-4">
+				<p className="text-xs text-first">© 1994 - {new Date().getFullYear()} | Soft Clever</p>
+				<Link to="/politica-de-privacidade" className="text-xs text-third">
 					Política de Privacidade
 				</Link>
 			</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Footer from "../components/Footer"
 import Menu from "../components/Menu"
+import Whatsapp from "../components/Whatsapp"
 
 function Home() {
 	return (
 		<>
 			<Menu />
 			<Footer />
+			<Whatsapp />
 		</>
 	)
 }
