c5e0f95e45452d0374ddff53945024e2d676560d
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2024-02-28 17:55:41 -0300

    ADD: DataCounters Component
    Added the DataCounters component to display dynamic data counters.
    
    The DataCounters component receives the following data as props: icon (icon to represent the counter), title (counter title), value (initial value of the counter), and classNames (additional CSS classes).
    Implemented logic to dynamically update the counter values when the component is visible on the user's screen.
    The displayed counters include information about users, clients, projects, and team.

---
+++ src/components/DataCounters.tsx
@@ -0,0 +1,103 @@
+import { ReactNode, useEffect, useRef, useState } from "react"
+import { FaUsers } from "react-icons/fa"
+import { FaRegUser } from "react-icons/fa6"
+import { GrUserManager } from "react-icons/gr"
+import { LuCable } from "react-icons/lu"
+
+const DataCounters = () => {
+	const DataCounter = ({ icon, title, value, classNames }: { icon: ReactNode; title: string; value: number; classNames?: string }) => {
+		const [isVisible, setIsVisible] = useState(false)
+		const dataCounterRef = useRef<HTMLDivElement>(null)
+
+		function ValueUpdater(finalValue: number): number {
+			const [currentValue, setCurrentValue] = useState(0)
+			const increment = finalValue / 60 // Calcula o incremento
+
+			useEffect(() => {
+				if (!isVisible) return // Não atualiza se não estiver visível
+
+				const interval = setInterval(() => {
+					setCurrentValue((prevValue) => {
+						const newValue = prevValue + increment
+						if (newValue >= finalValue) {
+							clearInterval(interval) // Para o intervalo quando o valor final for alcançado
+							return finalValue
+						} else {
+							return Math.round(newValue) // Arredonda o valor para conter valores decimais
+						}
+					})
+				}, 16) // Intervalo de atualização em milissegundos
+
+				return () => clearInterval(interval) // Limpa o intervalo quando o componente é desmontado
+			}, [finalValue, increment, isVisible]) // Executa o efeito sempre que o valor final ou a visibilidade mudar
+
+			return currentValue
+		}
+
+		useEffect(() => {
+			const options = {
+				root: null,
+				rootMargin: "0px",
+				threshold: 0.1, // Define o limiar de visibilidade para 10%
+			}
+
+			const observer = new IntersectionObserver((entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						setIsVisible(true) // Define como visível quando estiver dentro da viewport
+					}
+				})
+			}, options)
+
+			if (dataCounterRef.current) {
+				observer.observe(dataCounterRef.current)
+			}
+
+			return () => {
+				if (dataCounterRef.current) {
+					observer.unobserve(dataCounterRef.current) // Para de observar quando o componente for desmontado
+				}
+			}
+		}, [])
+
+		return (
+			<div ref={dataCounterRef} className={`${classNames} flex w-full gap-4 sm:max-w-[50%] lg:w-[25%] lg:flex-grow`}>
+				<div className="flex h-12 w-12 items-center justify-center rounded bg-second">{icon}</div>
+				<div className="flex flex-grow flex-col">
+					<p className="w-full font-bold text-second">{title.toUpperCase()}</p>
+					<p className="w-full font-bold text-third">
+						{ValueUpdater(value)}
+						<span className="font-bold text-second"></span>
+					</p>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<>
+			<div className="flex w-full flex-wrap">
+				<DataCounter
+					classNames="py-4 sm:pr-4 sm:border-r-1 border-[#bcced8] "
+					icon={<FaRegUser className="text-white" size={20} />}
+					title="USUÁRIOS"
+					value={12000}
+				/>
+				<DataCounter
+					classNames="py-4 sm:pl-4 lg:border-r-1 border-[#bcced8]"
+					icon={<FaUsers className="text-white" size={25} />}
+					title="CLIENTES"
+					value={3000}
+				/>
+				<DataCounter
+					classNames="py-4 sm:pr-4 sm:border-r-1 border-[#bcced8] lg:pl-4"
+					icon={<LuCable className="text-white" size={23} />}
+					title="PROJETOS"
+					value={1000}
+				/>
+				<DataCounter classNames="py-4 sm:pl-4" icon={<GrUserManager className="text-white" size={23} />} title="NOSSA EQUIPE" value={30} />
+			</div>
+		</>
+	)
+}
+export default DataCounters
--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 function Whatsapp() {
 	return (
 		<Link
-			className="absolute bottom-4 right-0 z-10 rounded-l-lg bg-white p-2"
+			className="fixed bottom-4 right-0 z-10 rounded-l-lg bg-white p-2"
 			to={"https://web.whatsapp.com/send?phone=5511992810249&text=Ol%C3%A1%2C%20Soft%20Clever.%20Gostaria%20de%20solicitar%20um%20or%C3%A7amento."}
 		>
 			<FontAwesomeIcon icon={faWhatsapp} style={{ color: "#25d366" }} size="2xl" />
