6e0c40ef6d573cd441eabd7f255d3fd932f0b024
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2024-03-08 14:27:41 -0300

    Remove Carousel
    Co-Authored-By: Carlos Augusto <40805641+cancastico@users.noreply.github.com>
    Co-Authored-By: Dayverson Miranda <104338781+dayve0@users.noreply.github.com>
    Co-Authored-By: Gabriel Lacerda <105825976+gabriel-lacerdaa@users.noreply.github.com>
    Co-Authored-By: Henrique Fontenele <101604488+henrique0078@users.noreply.github.com>
    Co-Authored-By: Mateus Sales <43645044+mateussales98@users.noreply.github.com>

--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -1,81 +1,34 @@
-import ClientsCarousel from "../components/Carousels/ClientsCarousel"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Menu from "../components/Menu"
 import Whatsapp from "../components/Whatsapp"
 
 function Clientes() {
-	const images = [
-		"ADRIANA HAIR.jpg",
-		"AIA.png",
-		"ALBUS-CLINIC.jpg",
-		"ALIBEC.jpeg",
-		"ALINHASTRO.jpg",
-		"ANDERSSON CABELOS.jpg",
-		"AQUI O BICHO PEGA.png",
-		"ART VENITE.png",
-		"ATIVA PORTARIA INTELIGENTE.png",
-		"BARBARA CONSERVAS.jpg",
-		"BAX ARTIGOS FESTAS.png",
-		"BICHO CHIC.jpg",
-		"BOLLATEL.png",
-		"BRASIL SISTEMAS.jpeg",
-		"BRUMED.png",
-		"CAVIVA 2.png",
-		"Colormix.png",
-		"COOPERPECAS.png",
-		"CSE.jpg",
-		"CSP FIX.jpeg",
-		"ENVELOPACK.png",
-		"ESCALEVE.jpeg",
-		"FASTLASER.jpeg",
-		"FENIXGAS.jpg",
-		"GOLDEN MIX.png",
-		"GR-Modas.jpg",
-		"INFINI.png",
-		"INFOLINE.png",
-		"IRMAOS BOZZA.jpg",
-		"JACC DOCES.jpg",
-		"JCRB BLOCOS.png",
-		"LIRA.png",
-		"logo-casa-do-bar.png",
-		"MAINARD.jpg",
-		"MIAMI BILHARES.png",
-		"MOTODAKAR.png",
-		"nani chinelato.jpg",
-		"NANO BLINDAGENS.png",
-		"OCULOS MANIA.png",
-		"PET SHOP BIORI.jpg",
-		"PONTO DO PADEIRO.png",
-		"PONTUAL POLIMEROS.png",
-		"portoro.png",
-		"PROLAR.jpg",
-		"RAMOS.jpg",
-		"RAMSOR.png",
-		"RED NOSE.jpg",
-		"RIKWILL.jpg",
-		"RIQUELLA.png",
-		"ROMA.jpg",
-		"RVC AUTOMACAO.jpeg",
-		"SALTOJA.jpg",
-		"TECNIGRAV.png",
-		"TRESSAR.png",
-		"TUGUMI.jpg",
-		"TUSSAR.jpg",
-	]
 	return (
 		<>
 			<Menu />
 			<Header title={"Nossos Clientes"} subtitle="PARCEIROS DE NEGÓCIOS" />
-			<ClientsCarousel images={images} />
 			<main className="flex w-full flex-col items-center justify-center gap-8">
-				<div className="item-center flex w-full flex-col justify-center gap-8 bg-background p-8 sm:p-16 lg:flex-row lg:gap-0">
-					<div className="flex w-full flex-col gap-8 sm:max-w-3xl lg:max-w-screen-lg lg:flex-row">
-						<p className="text-white">
-							Nós da Soft Clever temos o intuito de ter sua empresa como membro do nosso grupo e não somente como mais um cliente.
-						</p>
+				<div className="item-center flex w-full flex-col justify-center gap-8 px-8 pb-8 sm:px-16 sm:pb-0 lg:flex-row lg:gap-0">
+					<div className="flex w-full flex-col items-center justify-center gap-8 sm:max-w-3xl lg:max-w-screen-lg lg:flex-row">
+						<div className="flex flex-col gap-6 pt-8">
+							<p className="w-full text-base font-medium text-[#3c4a54]">
+								Nós da Soft Clever temos o intuito de ter sua empresa como membro do nosso grupo e não somente como mais um cliente.
+							</p>
+							<h2 className="w-full pt-4 text-3xl font-black text-second">Veja abaixo as avaliações recebidas:</h2>
+							<p className="w-full text-base font-medium text-[#3c4a54]">
+								A Soft Clever desenvolve soluções em sistemas desde 1994. São mais de 12.000 usuários atendidos ao longo destes 30 anos e
+								centenas de parcerias firmadas com contabilidades.
+							</p>
+						</div>
+						<img
+							className="hidden w-1/3 lg:block"
+							src="assets/images/png/clients.png"
+							alt="Um homem e uma mulher com roupas sociais apertando as mãos"
+						/>
 					</div>
 				</div>
+				<div className="item-center flex w-full flex-col justify-center gap-8 p-8 sm:p-16"></div>
 			</main>
 			<Footer />
 			<Whatsapp />
