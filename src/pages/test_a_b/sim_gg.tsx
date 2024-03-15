246be74e9f4dd69cd1b2d9131e21ee2b8b87b8a0
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2024-03-15 10:24:19 -0300

    sim_gg
    with gg sim
    
    Co-Authored-By: Carlos Augusto <40805641+Cancastico@users.noreply.github.com>
    Co-Authored-By: Dayverson Miranda <104338781+Dayve0@users.noreply.github.com>
    Co-Authored-By: Henrique Fontenele <101604488+Henrique0078@users.noreply.github.com>
    Co-Authored-By: Caio Freire <caiohenriquefreire@icloud.com>
    Co-Authored-By: Gabriel Lacerda <105825976+gabriel-lacerdaa@users.noreply.github.com>
    Co-Authored-By: Mateus Sales <43645044+mateussales98@users.noreply.github.com>

---
+++ src/pages/test_a_b/sim_gg.tsx
@@ -0,0 +1,45 @@
+import DataCounters from "../../components/DataCounters"
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+import Whatsapp from "../../components/Whatsapp"
+import Menu2 from "./Menu2"
+
+function ASoftClever2() {
+	return (
+		<>
+			<Menu2 />
+			<Header title={"A Soft Clever"} subtitle="Nossos valores" />
+			<main className="flex w-full flex-col items-center justify-center p-8">
+				<div className="item-center flex w-full flex-col justify-center gap-8 sm:max-w-3xl lg:max-w-screen-lg lg:flex-row lg:gap-0">
+					<div className="flex w-full flex-col gap-8 sm:max-w-3xl lg:max-w-screen-lg lg:flex-row">
+						<div className="flex w-full flex-col gap-8 self-center lg:max-w-[67%] lg:flex-row">
+							<p className="w-full text-base font-medium text-[#3c4a54]">
+								Desde 1994, a Soft Clever tem sido uma referência em desenvolvimento de sistemas. Com mais de 12.000 clientes satisfeitos e
+								parcerias estabelecidas com escritórios de contabilidade, comprometemo-nos em fornecer serviços cada vez melhores. Nosso
+								compromisso com a excelência e o foco no bem-estar dos clientes são os pilares que norteiam nossa atuação. Isso se reflete na
+								confiança e satisfação demonstradas por nossos clientes em cada interação.
+							</p>
+							<p className="w-full text-base font-medium text-[#3c4a54]">
+								Contamos com uma equipe própria e altamente qualificada, pronta para atender às diversas demandas do mercado com agilidade e
+								dedicação. A fidelidade de nossos clientes é o maior reconhecimento da qualidade de nossos serviços. Desenvolvemos soluções
+								personalizadas para empresas de todos os portes, adaptando-nos às necessidades específicas de cada cliente.
+							</p>
+						</div>
+						<div className="flex w-full flex-col gap-8 lg:max-w-[33%]">
+							<img
+								src="assets/images/jpg/a-soft-clever.jpg"
+								alt="Dois homens vestidos de terno e roupas sociais, apertando as mãos após fechar um acordo bem-sucedido"
+								className="w-full"
+							/>
+						</div>
+					</div>
+				</div>
+				<DataCounters />
+			</main>
+			<Footer />
+			<Whatsapp />
+		</>
+	)
+}
+
+export default ASoftClever2
